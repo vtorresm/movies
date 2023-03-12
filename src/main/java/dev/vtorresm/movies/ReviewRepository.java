dc50a1f9d9f36c7d9313d6889812b84b7ccfb84c
Victor Torres <vatorresm88@gmail.com>
2023-03-12 16:06:30 -0500

    Clase ReviewService

--- src/main/java/dev/vtorresm/movies/ReviewRepository.java
+++ src/main/java/dev/vtorresm/movies/ReviewRepository.java
@@ -1,4 +1,9 @@
 package dev.vtorresm.movies;
 
-public interface ReviewRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
