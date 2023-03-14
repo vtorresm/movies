a31c3c0396733f8d5a1c31f7517bbe6024a6850d
Victor Torres <vatorresm88@gmail.com>
2023-03-14 15:32:50 -0500

    Clase ReviewController.java

--- src/main/java/dev/vtorresm/movies/ReviewController.java
+++ src/main/java/dev/vtorresm/movies/ReviewController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
     @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get(imdbId)), HttpStatus.CREATED);
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
     }
 }
